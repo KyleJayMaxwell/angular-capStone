a6b2c1a904337ca2ea9db004d88301e3f5f2b11a
Kyle Maxwell <kylejaymaxwell@gmail.com>
2016-06-04 12:09:53 -0600

    separation of layout files

---
+++ src/client/router.js
@@ -0,0 +1,23 @@
+(function () {
+
+  'use strict';
+
+  angular.module('currentAmerica')
+    .config(config)
+    .run(routeChange);
+
+  config.$inject = ['$routeProvider', '$httpProvider'];
+  routeChange.$inject = ['$rootScope', '$location', '$window'];
+
+
+  function config($routeProvider, $httpProvider) {
+    $routeProvider
+    .when('/', {
+      template: '<landing></landing>',
+
+    })
+    .when('/home', {
+      template: '<members-all></members-all>',
+
+    })
+  }
